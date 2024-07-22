e9938540e28509ddf5c520075ba0b6a1920760ec
anajaramillo37 <64598509+anajaramillo37@users.noreply.github.com>
2024-07-22 22:25:08 +0200

    Update ranking.py

--- mammoth/datasets/ranking.py
+++ mammoth/datasets/ranking.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from mammoth.models.model import Model
 from loader_data_csv_rankings import data_csv_rankings
 
@@ -7,16 +8,22 @@ class Ranking(Model):
     def __init__(self, path: str):
         self.model_url = path
 
-    def normal_ranking(self,
-                       path,
-                       ranking_variable='citations'):
-        '''Ranking without considering any prottected attribute and just one variable
-        '''
+    def normal_ranking(self, path: str, ranking_variable: str = 'citations') -> pd.DataFrame:
+        """
+        Perform ranking without considering any protected attribute and using only one variable.
 
-        dataframe_ranking = normal_ranking(dataset, variable)
+        Args:
+        path (str): Path to the dataset CSV file.
+        ranking_variable (str): Variable to use for ranking.
 
-        dataframe = data_csv_rankings(path)
-        rankend_dataframe = dataframe.sort_values(ranking_variable)
-        rankend_dataframe['Ranking'] = [i+1 for i in range(rankend_dataframe.shape[0])]
-    
-        return rankend_dataframe
+        Returns:
+        pd.DataFrame: DataFrame with rankings based on the specified variable.
+        """
+        # Load the dataset
+        dataset = data_csv_rankings(path)
+        
+        # Perform the ranking
+        ranked_dataframe = dataset.sort_values(ranking_variable)
+        ranked_dataframe['Ranking'] = [i + 1 for i in range(ranked_dataframe.shape[0])]
+        
+        return ranked_dataframe
