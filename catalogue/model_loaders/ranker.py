f9ce9bb5e73b3b9bdc5577567a5e9bd536bf6a02
anajaramillo37 <64598509+anajaramillo37@users.noreply.github.com>
2024-07-23 12:21:43 +0200

    Update ranker.py

--- catalogue/model_loaders/ranker.py
+++ catalogue/model_loaders/ranker.py
@@ -1,18 +1,21 @@
 from mammoth.datasets import CSV
 from mammoth.exports import Markdown
 from typing import Dict, List
-from mammoth.integration import metric
-from mammoth.integration import loader
-from mammoth.models import onnx
+from mammoth.integration import metric, loader
 from loader_data_csv_rankings import data_csv_rankings
-from Rankings import normal_ranking
+from mammoth.datasets.rankings import normal_ranking
 
 
 @loader(namespace="mammotheu", version="v003", python="3.11")
-def normal_ranking_model(
+def load_normal_ranking_model(
     path: str,
-    ranking_variable: str,
+    ranking_variable: str
 ) -> normal_ranking:
-    '''This is a Ranking loader
-    '''
-    return normal_ranking(path,ranking_variable)
+    """
+    Load a normal ranking model.
+
+    :param path: The path to the model file.
+    :param ranking_variable: The variable used for ranking.
+    :return: The loaded normal ranking model.
+    """
+    return normal_ranking(path, ranking_variable)
