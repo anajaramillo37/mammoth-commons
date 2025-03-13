2d798edaef9f4a7938c3ce9536592cf39c4f1a5f
maniospas <maniospas@hotmail.com>
2025-03-13 06:30:42 +0200

    trying to fix doc generation

--- mai/__init__.py
+++ mai/__init__.py
@@ -1,3 +1,7 @@
 from mai import backend
-from mai import states
-from mai import bias
+
+try:
+    from mai import states
+    from mai import bias
+except ImportError:
+    print("Failed to import MAI-Bias frontend (you are probably lacking a graphics environment)")
